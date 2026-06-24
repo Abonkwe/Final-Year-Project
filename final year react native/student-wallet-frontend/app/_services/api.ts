9b36a2e5299681bc06b569e52a82260c3f354e36
Abonkwe <abonkweprincely2005@gmail.com>
2026-06-24 08:41:14 +0100

    Added everything

--- final year react native/student-wallet-frontend/app/_services/api.ts
+++ final year react native/student-wallet-frontend/app/_services/api.ts
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Axios client module instance targeting the student wallet backend
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: 'http://127.0.0.1:8000/wallet_api/v1',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
